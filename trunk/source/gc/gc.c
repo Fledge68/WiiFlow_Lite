ec4692fb41e0e20052658dab1190b64ac84c1515
fix94.1@gmail.com <fix94.1@gmail.com@81555f4c-30bf-4792-b450-80a1b71d4377>
2012-02-02 22:22:46 +0000

    -added option to set DML game lanugage, gamecube games will now use the language of the wii

--- trunk/source/gc/gc.c
+++ trunk/source/gc/gc.c
@@ -26,6 +26,13 @@
 #define VIDEO_MODE_NTSC480P 3
 #define VIDEO_MODE_PAL480P 4
 
+#define SRAM_ENGLISH 0
+#define SRAM_GERMAN 1
+#define SRAM_FRENCH 2
+#define SRAM_SPANISH 3
+#define SRAM_ITALIAN 4
+#define SRAM_DUTCH 5
+
 syssram* __SYS_LockSram();
 u32 __SYS_UnlockSram(u32 write);
 u32 __SYS_SyncSram(void);
@@ -36,7 +43,7 @@ void set_video_mode(int i)
 	sram = __SYS_LockSram();
 	void *m_frameBuf;
 	static GXRModeObj *rmode;
-	if (i == 0)
+	if (i == VIDEO_MODE_NTSC)
 	{
 		rmode = &TVNtsc480IntDf;
 		sram->flags = sram->flags & ~(1 << 0);	// Clear bit 0 to set the video mode to NTSC
@@ -58,4 +65,40 @@ void set_video_mode(int i)
 	
 	VIDEO_ClearFrameBuffer(rmode, m_frameBuf, COLOR_BLACK);
 	VIDEO_SetNextFramebuffer(m_frameBuf);
+}
+
+u8 get_wii_language()
+{
+	switch (CONF_GetLanguage())
+	{
+		case CONF_LANG_GERMAN:
+			return SRAM_GERMAN;
+		case CONF_LANG_FRENCH:
+			return SRAM_FRENCH;
+		case CONF_LANG_SPANISH:
+			return SRAM_SPANISH;
+		case CONF_LANG_ITALIAN:
+			return SRAM_ITALIAN;
+		case CONF_LANG_DUTCH:
+			return SRAM_DUTCH;
+		default:
+			return SRAM_ENGLISH;
+	}
+}
+
+void set_language(u8 lang)
+{
+	if (lang == 0)
+	{
+		lang = get_wii_language();
+	}
+	else
+		lang--;
+
+	syssram *sram;
+	sram = __SYS_LockSram();
+	sram->lang = lang;
+
+	__SYS_UnlockSram(1); // 1 -> write changes
+	while(!__SYS_SyncSram());
 }
