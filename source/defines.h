1a4626af4357ea303cd6a4f99bbe138a31090b8b
fedeanto11 <fedeanto11@hotmail.it>
2012-02-04 17:18:15 +0000

    * Fixed the menu_about issue ( Now all is displayed correctly) * Removed FIX94 because now he's already on devs ( Probably that was the issue LOL )

--- source/defines.h
+++ source/defines.h
@@ -18,16 +18,27 @@
 #define LOADER_AUTHOR			"Kwiirk, Waninkoko, Hermes"
 #define GUI_AUTHOR				"Hibernatus"
 
-#define THANKS	\
-"Lustar, CedWii, Benjay, Domi78, Oops, Celtiore, Jiiwah, FluffyKiwi, Roku93, \
-Spayrosam, Bluescreen81, Chappy23, BlindDude, Bubba, DJTaz, OggZee, Usptactical, \
-WiiPower, Hermes, Spidy1000, Dimok, Kovani, Drexyl, DvZ, Etheboss, FIX94, GaiaKnight, \
-nibb, NJ7, Plasma, Pakatus, wolf, ravmn, spidercaleb, Ziggy34, xFede, entropy \
-And to anyone who has donated or contributed to Wiiflow \
-that we missed!"
+#define THANKS \
+"Lustar, CedWii, Benjay, Domi78, Oops, \
+Celtiore, Jiiwah, FluffyKiwi, Roku93, \
+Spayrosam, Bluescreen81, Chappy23, \
+BlindDude, Bubba, DJTaz, OggZee, entropy,\
+Usptactical, WiiPower, Hermes, Spidy1000, \
+Dimok, Kovani, Drexyl, DvZ, Etheboss, \
+GaiaKnight, nibb, NJ7, Plasma, Pakatus, \
+wolf, ravmn, spidercaleb, Ziggy34, xFede, \
+And to anyone who has donated or \
+contributed to Wiiflow that we missed!"
 
-#define THANKS_SITES			"devkitpro.org, wiibrew.org, gametdb.com, ohloh.net, wiifanart.com, wiiflowiki.com, tgames.fr.nf"
-#define THANKS_CODE				"CFG Loader, uLoader, USB Loader GX, NeoGamma, WiiXplorer, Triiforce, Mighty Channels"
+#define THANKS_SITES \
+"devkitpro.org, wiibrew.org, gametdb.com, \
+ohloh.net, wiifanart.com, wiiflowiki.com, \
+tgames.fr.nf"
+
+#define THANKS_CODE \
+"CFG Loader, uLoader, USB Loader GX, \
+NeoGamma, WiiXplorer, Mighty Channels, \
+Triiforce" 
 
 #define WIINNERTAG_URL 			"http://www.wiinnertag.com/wiinnertag_scripts/update_sign.php?key={KEY}&game_id={ID6}"
 #define DUTAG_URL				"http://tag.darkumbra.net/{KEY}.update={ID6}"
