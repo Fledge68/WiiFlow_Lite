151eca94a29424a2535172b793d1ba86629535eb
fledge68 <fledge68@gmail.com>
2012-03-01 16:13:24 +0000

    - last theme layout fix that I missed way back in r101

--- source/menu/menu_game.cpp
+++ source/menu/menu_game.cpp
@@ -379,7 +379,7 @@ void CMenu::_game(bool launch)
 		{
 			s8 direction = BTN_1_PRESSED ? 1 : -1;
 			const char *domain = _domainFromView();
-			int cfVersion = loopNum(m_cfg.getInt(domain, "last_cf_mode" , 1) + direction, m_numCFVersions);
+			int cfVersion = 1+loopNum((m_cfg.getInt(domain, "last_cf_mode", 1)-1) + direction, m_numCFVersions);
 			_loadCFLayout(cfVersion);
 			m_cf.applySettings();
 			m_cfg.setInt(domain, "last_cf_mode" , cfVersion);
