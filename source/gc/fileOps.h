fcf9d344f789c86dae9bcca4a44879b754da74d8
fix94.1 <fix94.1@gmail.com>
2012-03-03 17:16:11 +0000

    -updated dml usb to sd installer, should be faster now thanks to threaded copy by stfour, also fixed codedumps when copying from ntfs to sd, everything should work now

--- source/gc/fileOps.h
+++ source/gc/fileOps.h
@@ -8,28 +8,6 @@ extern "C"
 
 typedef void (*progress_callback_t)(int status,int total,void *user_data);
 
-typedef struct 
-	{
-	u64 size, bytes; // for operation that uses more than one file
-	u32 startms;
-	}
-s_fsopmulty;
-
-typedef struct 
-	{
-	char op[256];	// Calling process can set filename or any other info that fit
-	
-	u32 size, bytes;
-
-	s_fsopmulty multy;
-	
-	int flag1;		// user defined flag
-	bool breakop;	// allow to stop a long operation
-	}
-s_fsop;
-
-extern s_fsop fsop;
-
 bool fsop_GetFileSizeBytes (char *path, size_t *filesize);
 u64 fsop_GetFolderBytes (char *source);
 u32 fsop_GetFolderKb (char *source);
