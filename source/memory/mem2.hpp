73e34da18826f717ab946ed32c151caa3e48b5a4
fix94.1 <fix94.1@gmail.com>
2012-05-07 19:45:25 +0000

    -removed our linker script and changed a few things about memory usage, please test if everything works fine, if memory still allocates fine and if memory usage is the same, if not we can revert it

--- source/memory/mem2.hpp
+++ source/memory/mem2.hpp
@@ -9,26 +9,30 @@ extern "C"
 {
 #endif
 
+#define MAX_MEM1_ARENA_LO	((void *) (((u32)APPLOADER_START)-size))
+#define MEM2_PRIORITY_SIZE	0x1000
+
+void *MEM1_alloc(unsigned int s);
+void *MEM1_realloc(void *p, unsigned int s);
+void MEM1_free(void *p);
+
 void MEM2_init(unsigned int mem2Size);
 void MEM2_cleanup(void);
 void MEM2_clear(void);
-void MEM1_cleanup(void);
-void MEM1_clear(void);
-void *MEM2_alloc(unsigned int s);
-void *MEM1_alloc(unsigned int s);
-void *MEM2_realloc(void *p, unsigned int s);
-void *MEM1_realloc(void *p, unsigned int s);
 void MEM2_free(void *p);
-void MEM1_free(void *p);
+void *MEM2_alloc(unsigned int s);
+void *MEM2_realloc(void *p, unsigned int s);
 unsigned int MEM2_usableSize(void *p);
 unsigned int MEM2_freesize();
-unsigned int MEM1_freesize();
 
 #ifdef __cplusplus
 }
 #endif
 
-
-enum Alloc { ALLOC_MALLOC, ALLOC_MEM2 };
+enum Alloc 
+{ 
+	ALLOC_MALLOC, 
+	ALLOC_MEM2, 
+};
 
 #endif // !defined(__MEM2_HPP)
