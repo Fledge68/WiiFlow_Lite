285e8ec753eb9318f657b623e2eddd9a6599c741
fix94.1 <fix94.1@gmail.com>
2012-11-29 18:48:44 +0000

    -added a wiimote battery information bar to the home menu under the buttons, it'll display the information in percent

--- source/const_str.hpp
+++ source/const_str.hpp
@@ -9,5 +9,5 @@
 
 static const string &VERSION_STRING = sfmt("%s (%s-r%s)", APP_NAME, APP_VERSION, SVN_REV);
 static const wstringEx SVN_REV_W(SVN_REV);
-
+static const wstringEx PLAYER_BATTERY_LABEL("P1 %003.f%% | P2 %003.f%% | P3 %003.f%% | P4 %003.f%%");
 #endif
