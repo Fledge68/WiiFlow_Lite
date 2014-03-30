c92f75eb9b1d8646f3e48817835595c6a881db6c
fix94.1@gmail.com <fix94.1@gmail.com@81555f4c-30bf-4792-b450-80a1b71d4377>
2014-03-30 21:45:47 +0000

    -changed up some booting code to hopefully fix those random freezes on game and homebrew bootup

--- trunk/resources/extldr/main.c
+++ trunk/resources/extldr/main.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "string.h"
 #include "sync.h"
 #include "usbgecko.h"
@@ -11,7 +9,7 @@ void _main(void)
 {
 	usbgecko_init();
 	gprintf("Copying External Booter...\n");
-	memcpy(start, buffer, 0xF0000); //960kb safe copying of booter
+	_memcpy(start, buffer, 0xF0000); //960kb safe copying of booter
 	sync_before_exec(start, 0xF0000);
 	gprintf("Done! Jumping to Entrypoint...\n");
 	exeEntryPoint();
