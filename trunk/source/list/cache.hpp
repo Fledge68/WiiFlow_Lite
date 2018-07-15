6fd4c52b7e9c29b741d82e7d0e2c28bf30155f6a
fledge68 <fledge68@81555f4c-30bf-4792-b450-80a1b71d4377>
2018-07-15 20:23:32 +0000

    -fixed sd only the correct way. now it really does work. -shrunk the list cache code.

--- trunk/source/list/cache.hpp
+++ trunk/source/list/cache.hpp
@@ -10,37 +10,25 @@
 //#include "gecko.hpp"
 using namespace std;
 
-const char io[4][5] = {
+const char io[2][3] = {
 	"wb",
 	"rb",
-	"ab",
-	"wb",
 };
 
 enum CMode
 {
 	SAVE,
 	LOAD,
-	ADD,
-	REMOVE
 };
 
 class CCache
 {
 	public:
-		 CCache(dir_discHdr &tmp, string path, u32 index, CMode mode);								/* Load/Save One */
-		 CCache(vector<dir_discHdr> &list, string path, CMode mode);								/* Load/Save All */
-		 CCache(vector<dir_discHdr> &list, string path, dir_discHdr tmp, CMode mode);						/* Add One */
-		 CCache(vector<dir_discHdr> &list, string path, u32 index, CMode mode);					/* Remove One */
+		 CCache(vector<dir_discHdr> &list, string path, CMode mode);
 		~CCache();
 	private:
 		void SaveAll(vector<dir_discHdr> list);
-		void SaveOne(dir_discHdr tmp, u32 index);
 		void LoadAll(vector<dir_discHdr> &list);
-		void LoadOne(dir_discHdr &tmp, u32 index);
-		
-		void AddOne(vector<dir_discHdr> &list, dir_discHdr tmp);
-		void RemoveOne(vector<dir_discHdr> &list, u32 index);
 
 		FILE *cache;
 		string filename;
