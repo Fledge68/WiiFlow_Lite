a0ef54af49f7e3ca1490b0ec1227cd290450233f
Fledge68 <fledge68@gmail.com>
2024-04-09 12:54:04 -0500

    - fix for 'auto' game IOS option. in v5.5.4 'auto' causes the game to use a d2x cios base 38 if you have it installed, which prevents game from booting. this fix prevents this from happening.

--- source/menu/menu_game_boot.cpp
+++ source/menu/menu_game_boot.cpp
@@ -777,7 +777,7 @@ int CMenu::_loadGameIOS(u8 gameIOS, int userIOS, const char *id, bool RealNAND_C
 		{
 			for(CIOSItr itr = _cios_base.begin(); itr != _cios_base.end(); itr++)
 			{
-				if(itr->first > gameIOS)//compare bases
+				if(itr->first > gameIOS && itr->first != 38)//compare bases
 				{
 					slot = itr->second;// set to cios slot
 					break;
